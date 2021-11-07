0f542197d5379156f8a421d00bc0b47998f60f5b
LikithSai-Muvvala <67479241+LikithSai-Muvvala@users.noreply.github.com>
2021-11-07 15:52:58 +0530

    Create LargestSumInContinousSubArray.java

---
+++ LargestSumInContinousSubArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LargestSumInContinousSubArray {
+    public static void main(String args[]) {
+      int[] vals = {-2,-3,5,6,-6,7,8,9};
+      largestSumInSubArray(vals);
+    }
+    
+    public static void largestSumInContinousSubArray(int[] vals) {
+        int sum=Integer.MIN_VALUE;
+        int currSum=0;
+        int startIndex=0;
+        int endIndex=0;
+        for(int idx=0; idx<vals.length; idx++) {
+            System.out.println(String.format("%d is at Index: %d", vals[idx], idx));
+            currSum =  currSum + vals[idx];
+            if(currSum > sum) {
+                endIndex=idx;
+                sum = currSum;
+            }
+            if(currSum < 1) {
+                startIndex=idx+1;
+                currSum=0;
+            }
+        }
+        System.out.println(String.format("StartIndex: %d, EndIndex: %d, Sum: %d", startIndex, endIndex, sum));
+    }
+}
